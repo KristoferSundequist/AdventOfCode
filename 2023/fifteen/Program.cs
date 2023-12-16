7508e3011f9dcedbaad6ec4269b1df0d80700861
kristofer <krajle@gmail.com>
2023-12-16 13:56:29 +0100

    15 and 16

---
+++ 2023/fifteen/Program.cs
@@ -0,0 +1,137 @@
+﻿var line = File.ReadAllLines("./data.txt");
+if (line.Length != 1) throw new Exception("Expected 1 line");
+var stepStrs = line[0].Split(",");
+var sum = 0;
+foreach (var step in stepStrs)
+{
+    sum += RunHash(step);
+}
+Console.WriteLine($"Result 1: {sum}");
+
+
+var boxes = Enumerable.Range(0, 256).Select(_ => new Box()).ToArray();
+var steps = stepStrs.Select(str => Step.FromString(str));
+foreach (var step in steps)
+{
+    var boxIndex = RunHash(step.Label);
+    if (step.Operation == Operation.Add)
+    {
+        boxes[boxIndex].Upsert(new Lense(step.Label, step.FocalLength ?? throw new Exception("didnt expect null")));
+    }
+    else if (step.Operation == Operation.Remove)
+    {
+        boxes[boxIndex].Remove(step.Label);
+    }
+    else
+    {
+        throw new Exception($"Unkown operation {step.Operation}");
+    }
+    // Console.WriteLine($"After \"{step.RawString}\", label: {step.Label}");
+    // PrintBoxes();
+}
+
+long focusingPowerSum = 0;
+for (var i = 0; i < boxes.Length; i++)
+{
+    focusingPowerSum += (i + 1) * boxes[i].GetFocusingPower();
+}
+Console.WriteLine($"Result 2: {focusingPowerSum}");
+
+int RunHash(string str)
+{
+    int currentValue = 0;
+    foreach (var c in str)
+    {
+        var asAscii = (int)c;
+        currentValue += asAscii;
+        currentValue *= 17;
+        currentValue = currentValue % 256;
+    }
+    return currentValue;
+}
+
+void PrintBoxes()
+{
+    for (var i = 0; i < boxes.Length; i++)
+    {
+        if (!boxes[i].IsEmpty())
+        {
+            Console.WriteLine($"Box {i}: {boxes[i].ToString()}");
+        }
+    }
+}
+
+public class Box
+{
+    private LinkedList<Lense> _lenses = new LinkedList<Lense>();
+
+    public void Remove(string label)
+    {
+        if (_lenses.Nodes().FirstOrDefault(n => n.Value.label == label) is LinkedListNode<Lense> node)
+        {
+            _lenses.Remove(node);
+        }
+    }
+
+    public bool IsEmpty() => _lenses.Count == 0;
+
+    public override string ToString() => string.Join(" ", _lenses.Select(l => l.ToString()));
+
+    public void Upsert(Lense newLense)
+    {
+        if (_lenses.Nodes().FirstOrDefault(n => n.Value.label == newLense.label) is LinkedListNode<Lense> node)
+        {
+            node.Value = newLense;
+        }
+        else
+        {
+            _lenses.AddLast(newLense);
+        }
+    }
+
+    public long GetFocusingPower()
+    {
+        var sum = 0;
+        var slot = 1;
+        foreach (var node in _lenses)
+        {
+            sum += slot * node.focalLength;
+            slot++;
+        }
+        return sum;
+    }
+
+}
+
+public record Lense(string label, int focalLength)
+{
+    public override string ToString() => $"[{label} {focalLength}]";
+}
+
+public record Step
+{
+    public required string RawString { get; set; }
+    public required string Label { get; set; }
+    public required Operation Operation { get; set; }
+    public int? FocalLength { get; set; } = null;
+
+    public static Step FromString(string str)
+    {
+        if (str.Split("=") is [string label, string focalLengthStr])
+        {
+            return new Step { RawString = str, Label = label, Operation = Operation.Add, FocalLength = int.Parse(focalLengthStr) };
+        }
+
+        if (str.EndsWith("-"))
+        {
+            return new Step { RawString = str, Label = str[..^1], Operation = Operation.Remove, FocalLength = null };
+        }
+
+        throw new ArgumentException("Couldnt parse step");
+    }
+}
+public enum Operation
+{
+    Add,
+    Remove
+}
---
+++ 2023/sixteen/Program.cs
@@ -0,0 +1,114 @@
+﻿var lines = File.ReadAllLines("data.txt");
+var contraption = new Dictionary<Coordinate, char>();
+for (var y = 0; y < lines.Length; y++)
+{
+    for (var x = 0; x < lines[0].Length; x++)
+    {
+        contraption.Add(new Coordinate(y, x), lines[y][x]);
+    }
+}
+
+var beams = GetAllBeams(contraption, new HashSet<LightBeam> { new LightBeam(new Coordinate(0, 0), Direction.Right) });
+Console.WriteLine($"Result 1: {beams.Select(beam => beam.coordinate).Distinct().Count()}");
+
+
+int mostSoFar = 0;
+for (var i = 0; i < lines.Length; i++)
+{
+    mostSoFar = new List<int> {
+        mostSoFar,
+        GetAllBeams(contraption, new HashSet<LightBeam> { new LightBeam(new Coordinate(i, 0), Direction.Right) }).Select(beam => beam.coordinate).Distinct().Count(),
+        GetAllBeams(contraption, new HashSet<LightBeam> { new LightBeam(new Coordinate(i, lines[0].Length - 1), Direction.Left) }).Select(beam => beam.coordinate).Distinct().Count(),
+        GetAllBeams(contraption, new HashSet<LightBeam> { new LightBeam(new Coordinate(0, i), Direction.Down) }).Select(beam => beam.coordinate).Distinct().Count(),
+        GetAllBeams(contraption, new HashSet<LightBeam> { new LightBeam(new Coordinate(lines[0].Length - 1, i), Direction.Up) }).Select(beam => beam.coordinate).Distinct().Count()
+    }.Max();
+}
+Console.WriteLine($"Result 2: {mostSoFar}");
+
+void DrawEnergized(HashSet<Coordinate> coordinates, int maxY, int maxX)
+{
+    for (var y = -10; y < maxY + 10; y++)
+    {
+        for (var x = -10; x < maxX + 10; x++)
+        {
+            if (coordinates.Contains(new Coordinate(y, x)))
+            {
+                Console.Write('#');
+            }
+            else
+            {
+                Console.Write('.');
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+HashSet<LightBeam> GetAllBeams(Dictionary<Coordinate, char> contraption, HashSet<LightBeam> start)
+{
+    HashSet<LightBeam> lightBeams = [];
+    HashSet<LightBeam> newLightBeams = start;
+    while (true)
+    {
+        HashSet<LightBeam> nextNewLightBeams = [];
+        foreach (var lightBeam in newLightBeams)
+        {
+            lightBeams.Add(lightBeam);
+            if (contraption.TryGetValue(lightBeam.coordinate, out var c))
+            {
+                var nextBeams = lightBeam.Next(c);
+                foreach (var beam in nextBeams)
+                {
+                    if (contraption.ContainsKey(beam.coordinate) && !lightBeams.Contains(beam))
+                    {
+                        nextNewLightBeams.Add(beam);
+                    }
+                }
+            }
+        }
+        if (nextNewLightBeams.Count == 0)
+        {
+            break;
+        }
+        newLightBeams = nextNewLightBeams;
+    }
+    return lightBeams;
+}
+
+
+
+record LightBeam(Coordinate coordinate, Direction direction)
+{
+    public LightBeam[] Next(char c) => (direction, c) switch
+    {
+        (Direction.Up, '.') => [new LightBeam(coordinate with { y = coordinate.y - 1 }, Direction.Up)],
+        (Direction.Left, '.') => [new LightBeam(coordinate with { x = coordinate.x - 1 }, Direction.Left)],
+        (Direction.Right, '.') => [new LightBeam(coordinate with { x = coordinate.x + 1 }, Direction.Right)],
+        (Direction.Down, '.') => [new LightBeam(coordinate with { y = coordinate.y + 1 }, Direction.Down)],
+        (Direction.Up, '/') => [new LightBeam(coordinate with { x = coordinate.x + 1 }, Direction.Right)],
+        (Direction.Left, '/') => [new LightBeam(coordinate with { y = coordinate.y + 1 }, Direction.Down)],
+        (Direction.Right, '/') => [new LightBeam(coordinate with { y = coordinate.y - 1 }, Direction.Up)],
+        (Direction.Down, '/') => [new LightBeam(coordinate with { x = coordinate.x - 1 }, Direction.Left)],
+        (Direction.Up, '\\') => [new LightBeam(coordinate with { x = coordinate.x - 1 }, Direction.Left)],
+        (Direction.Left, '\\') => [new LightBeam(coordinate with { y = coordinate.y - 1 }, Direction.Up)],
+        (Direction.Right, '\\') => [new LightBeam(coordinate with { y = coordinate.y + 1 }, Direction.Down)],
+        (Direction.Down, '\\') => [new LightBeam(coordinate with { x = coordinate.x + 1 }, Direction.Right)],
+        (Direction.Up, '-') => [new LightBeam(coordinate with { x = coordinate.x - 1 }, Direction.Left), new LightBeam(coordinate with { x = coordinate.x + 1 }, Direction.Right)],
+        (Direction.Left, '-') => [new LightBeam(coordinate with { x = coordinate.x - 1 }, Direction.Left)],
+        (Direction.Right, '-') => [new LightBeam(coordinate with { x = coordinate.x + 1 }, Direction.Right)],
+        (Direction.Down, '-') => [new LightBeam(coordinate with { x = coordinate.x - 1 }, Direction.Left), new LightBeam(coordinate with { x = coordinate.x + 1 }, Direction.Right)],
+        (Direction.Up, '|') => [new LightBeam(coordinate with { y = coordinate.y - 1 }, Direction.Up)],
+        (Direction.Left, '|') => [new LightBeam(coordinate with { y = coordinate.y - 1 }, Direction.Up), new LightBeam(coordinate with { y = coordinate.y + 1 }, Direction.Down)],
+        (Direction.Right, '|') => [new LightBeam(coordinate with { y = coordinate.y - 1 }, Direction.Up), new LightBeam(coordinate with { y = coordinate.y + 1 }, Direction.Down)],
+        (Direction.Down, '|') => [new LightBeam(coordinate with { y = coordinate.y + 1 }, Direction.Down)],
+        _ => throw new Exception($"unhandled case ({direction}, {c})")
+    };
+}
+record Coordinate(int y, int x);
+enum Direction
+{
+    Up,
+    Left,
+    Right,
+    Down
+};
